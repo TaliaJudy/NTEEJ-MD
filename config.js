f027f15bafaf94b28be53d7388b3d9d500df15b1
Cytrak <149499127+Cytrak@users.noreply.github.com>
2024-08-21 05:32:46 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,14 +4,14 @@ if (fs.existsSync(".env"))
   require("dotenv").config({ path: __dirname + "/.env" });
 
 //=======[dependencies]====================//
-global.SESSION_ID = process.env.SESSION_ID || "";
+global.SESSION_ID = process.env.SESSION_ID || "eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoic01LYnZXSTYrT0ZGMlY4dzBURWxyT0dyNUVnaFJnaFk5TThxTmxFdmszQT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiZkpMUWR5ODRkOVhkbmNGaXBmazJmUTJLUnNNVm9JNjN5UWVIeWlGVXloYz0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJBRFVCUHlab2M1TmtoclJQM2F5ZGJRTzhha1RTQnVDUFprY0ZIKzlzbDM4PSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJzWnhKbkNNT0VNZ0ZPK2xvM3oxU3BWRThuRVRmdTlGMFU0dVBSdFc0V0U4PSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6Ik1KUDR4N0FWc3JrbHBxOVZ5SHVrMmsvb2NESzZKcEZkeTBBbmVNSGpMVjA9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjYwS3VuNHRnd3duYzl5NFoweG1lOGhjaWM1Vm4wVzZLTmVOWEhTUDhkVlE9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiY0oyTHROcWpOeG5LUnFFNVNJeklrZkRQN1ZpeXpmRnE0VWNid2Nac1RsUT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiUHpkdCttYUlIZEkxMTRRVDN2STh1V0ErWjYxaGJ5alZNTnR2MTZVaEQzRT0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IlM1Z2d0NW1nYzRZdFhTRWhTNXV2d01CSldod3dXcnZubXE3clozblhKRXRjWDhBb2NxcC9rQ3V1WExiMjBPUFk2QmFyRzdGaGdUUC9nS2docXRBNWdBPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MTU4LCJhZHZTZWNyZXRLZXkiOiIxWDZYaXBNWEc0UkN5aWc3YitWYVVSTGlIbmR3a2tuR3g1RWRoMGNwa0trPSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W3sia2V5Ijp7InJlbW90ZUppZCI6IjI1NDcxNTE1NTE5NkBzLndoYXRzYXBwLm5ldCIsImZyb21NZSI6dHJ1ZSwiaWQiOiI0RDgyMjVFNjQ5RTU4NDdFMDA0QUY4MTNFOUE3NUFGMSJ9LCJtZXNzYWdlVGltZXN0YW1wIjoxNzI0MjA3NDE2fV0sIm5leHRQcmVLZXlJZCI6MzEsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjozMSwiYWNjb3VudFN5bmNDb3VudGVyIjowLCJhY2NvdW50U2V0dGluZ3MiOnsidW5hcmNoaXZlQ2hhdHMiOmZhbHNlfSwiZGV2aWNlSWQiOiJNSDJFMWJhNlRJT21Tb1NCRVhTVEZBIiwicGhvbmVJZCI6Ijk4ZjFkMjA0LTg3YWItNGM1Yi1hYzUxLTljYzA4ZDc0MzdmMiIsImlkZW50aXR5SWQiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJiTzJBaGJ2c2pDS2N2cjVlQVJpYnpSblZUbTA9In0sInJlZ2lzdGVyZWQiOnRydWUsImJhY2t1cFRva2VuIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiUkJjQ1h1V2dzeHBnYmwwSzZEcTArMEtvZVNjPSJ9LCJyZWdpc3RyYXRpb24iOnt9LCJwYWlyaW5nQ29kZSI6IkhZTTFCUjdNIiwibWUiOnsiaWQiOiIyNTQ3MTUxNTUxOTY6MzBAcy53aGF0c2FwcC5uZXQifSwiYWNjb3VudCI6eyJkZXRhaWxzIjoiQ1AyN3FlTUNFS21pbGJZR0dDTWdBQ2dBIiwiYWNjb3VudFNpZ25hdHVyZUtleSI6IjdVcFJ1SHJsLzFRQ2p4eUhlR29BUm51cDhCVkZ1OTA4ZVdoUlRrTWJGanM9IiwiYWNjb3VudFNpZ25hdHVyZSI6IlNTcE53VXBtbnlEeUhLMXp1YWNNaWwrRTJ2NkkwMGFtNGYzUHdhQWpBTUltVDRzRThHVjBvQld2MGNudTJXUUlPdGprT2N6bUNwSkVKRGY3RmdiN0JnPT0iLCJkZXZpY2VTaWduYXR1cmUiOiIwMDhyNk00ZW16ck45d0d4SklPWnhHSlpvYVBYVExWUzlDMk5XZktkOVV1TUpiOE1TUHB4RjFtRmFFMXMwZjYvTkFEZnRZcm9HZ0VjNnpya3FzWFRpUT09In0sInNpZ25hbElkZW50aXRpZXMiOlt7ImlkZW50aWZpZXIiOnsibmFtZSI6IjI1NDcxNTE1NTE5NjozMEBzLndoYXRzYXBwLm5ldCIsImRldmljZUlkIjowfSwiaWRlbnRpZmllcktleSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkJlMUtVYmg2NWY5VUFvOGNoM2hxQUVaN3FmQVZSYnZkUEhsb1VVNURHeFk3In19XSwicGxhdGZvcm0iOiJhbmRyb2lkIiwibGFzdEFjY291bnRTeW5jVGltZXN0YW1wIjoxNzI0MjA3NDE0LCJteUFwcFN0YXRlS2V5SWQiOiJBQUFBQUR1eiJ9";
 global.MONGODB = process.env.MONGODB_URI || "";
 global.DATABASE_URL = process.env.DATABASE_URL || "";
 global.sudo = process.env.SUDO
   ? process.env.SUDO.replace(/[\s+]/g, "")
   : "null";
 global.owner = process.env.OWNER_NUMBER
-  ? process.env.OWNER_NUMBER.replace(/[\s+]/g, "")
+  ? process.env.OWNER_NUMBER.replace(/[\s+]/g, "254715155196")
   : "263714497545";
 global.THUMB_IMAGE =
   process.env.THUMB_IMAGE ||
@@ -31,7 +31,7 @@ module.exports = {
   author: process.env.PACK_AUTHER || "NTEEJ-MD",
   packname: process.env.PACK_NAME || "NTEEJ",
   botname: process.env.BOT_NAME || "NTEEJ-MD",
-  ownername: process.env.OWNER_NAME || "Nteej",
+  ownername: process.env.OWNER_NAME || "Cytra",
   errorChat: process.env.ERROR_CHAT || "",
   KOYEB_API: process.env.KOYEB_API || "false",
   REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "",
@@ -55,7 +55,7 @@ global.timezone = process.env.TZ || process.env.TIME_ZONE || "Africa/Lagos";
 global.github = process.env.GITHUB || "https://github.com/Ntee-j01/NTEEJ-MD";
 global.gurl = process.env.GURL || "https://whatsapp.com/channel/0029Vae3GZF9Bb658QgSCl1I";
 global.website = process.env.GURL || "https://chat.whatsapp.com/Er6RNNNVWV5LORN9Nr6hL7";
-global.devs = "263714497545";
+global.devs = "254715155196";
 global.msg_style = process.env.STYLE || "4";
 global.session_reset = process.env.SS_RESET || "false";
 global.gdbye = process.env.GOODBYE || "false";
@@ -68,7 +68,7 @@ global.waPresence = process.env.WAPRESENCE || "null";
 global.readcmds = process.env.READ_COMMAND || "false";
 global.readmessage = process.env.READ_MESSAGE || "false";
 global.readmessagefrom = process.env.READ_MESSAGE_FROM || "null";
-global.read_status = process.env.AUTO_READ_STATUS || "false";
+global.read_status = process.env.AUTO_READ_STATUS || "true";
 global.save_status = process.env.AUTO_SAVE_STATUS || "false";
 global.save_status_from = process.env.SAVE_STATUS_FROM || "null";
 global.read_status_from = process.env.READ_STATUS_FROM || "null";
